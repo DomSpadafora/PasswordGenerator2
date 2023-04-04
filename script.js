76cd4973a4426e747b8847bb9eacbef610b157ef
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-04-03 21:01:45 -0400

    number prompt

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 //great function for generating a password
 
 const passwordGenerator = () => {
-  let length = prompt('how long would you like your password to be?');
+  let length = prompt('How long would you like your password to be?');
     if (length <8 || length > 128){
       alert('Password nees to be between 8-128')
       return passwordGenerator();
     }
+
+    let input = "";
+    let passowrd = "";
+
+    let number = confirm('Would you like a number?')
+    if(number) input += '0123456789';
+
+    let 
 
   
     
